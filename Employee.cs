57d778d9a4b572bce5301f004fd24c2dfce92e09
Rashmika0388 <101638418+Rashmika0388@users.noreply.github.com>
2022-07-08 15:55:14 +0530

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+// edit the file
 namespace ORMFrameWork.Models
 {
     [Table("emp10")]
